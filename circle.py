c48de6681563e807d3366528923ad946be7e66a3
Zhiji022 <jimmydzj2006@gmail.com>
2018-11-21 14:22:51 -0800

    2 files

---
+++ circle.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+def circleArea(r):
+    return(math.pi*r**2)
+
+areaCircle(1)
+
---
+++ oval.py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+def ovalArea(a,b):
+    return(math.pi*a*b)
+
